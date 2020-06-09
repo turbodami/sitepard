470e495e29eddf380186cf33b82b2957ca89cb00
turbodami <presadamiano@gmail.com>
2020-06-09 20:19:45 +0200

    dio mucosssss

--- builder/pages/index.js
+++ builder/pages/index.js
@@ -1,27 +1,33 @@
-import Head from 'next/head';
+import Head from "next/head";
 
 const Index = () => (
+  <>
     <Head>
-         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/normalize/7.0.0/normalize.css" />
-      <link href="https://fonts.googleapis.com/css?family=Raleway" rel="stylesheet"></link>
-
+      <link
+        rel="stylesheet"
+        href="https://cdnjs.cloudflare.com/ajax/libs/normalize/7.0.0/normalize.css"
+      />
+      <link
+        href="https://fonts.googleapis.com/css?family=Raleway"
+        rel="stylesheet"
+      ></link>
     </Head>
-    <>
-        <div>
-        <h1>Dio lurido</h1>
-        <style jsx>
-            {`
-            div {
-                background: #333;
-            }
 
-            h1 {
-                background: #222;
-            }
-            `}
-        </style>
-        </div>
-    </>
+    <div>
+      <h1>Dio lurido</h1>
+      <style jsx>
+        {`
+          div {
+            background: #333;
+          }
+
+          h1 {
+            background: #222;
+          }
+        `}
+      </style>
+    </div>
+  </>
 );
 
 export default Index;
