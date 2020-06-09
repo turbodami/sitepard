ae6f5e57d54a6a03de38baebbd6d54b60845291c
turbodami <presadamiano@gmail.com>
2020-06-09 18:04:33 +0200

    dio fumatossssaa

--- user_site/pages/index.js
+++ user_site/pages/index.js
@@ -1,25 +1,25 @@
 import Link from "next/link";
 
-export default function Index = () => (
-  <ul>
-    <li>
-      <Link href="/b" as="/a">
-        <a>a</a>
-      </Link>
-    </li>
+export default function Index() {
+  return (
+    <ul>
+      <li>
+        <Link href="/b" as="/a">
+          <a>a</a>
+        </Link>
+      </li>
 
-    <li>
-      <Link href="/a" as="/b">
-        <a>b</a>
-      </Link>
-    </li>
-  </ul>
-);
-
+      <li>
+        <Link href="/a" as="/b">
+          <a>b</a>
+        </Link>
+      </li>
+    </ul>
+  );
+}
 /* Index.getInitialProps = async ({ query: { diocan } }) => {
   //console.log(diocan);
   return {
     diocan: diocan,
   };
 }; */
-
