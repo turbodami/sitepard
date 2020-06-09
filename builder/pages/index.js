698f6a2d95a0f49710374f8a54ae93ce48f886c6
turbodami <presadamiano@gmail.com>
2020-06-09 23:13:54 +0200

    prima di fare il casino

--- builder/pages/index.js
+++ builder/pages/index.js
@@ -1,33 +1,7 @@
-import Head from "next/head";
-
 const Index = () => (
-  <>
-    <Head>
-      <link
-        rel="stylesheet"
-        href="https://cdnjs.cloudflare.com/ajax/libs/normalize/7.0.0/normalize.css"
-      />
-      <link
-        href="https://fonts.googleapis.com/css?family=Raleway"
-        rel="stylesheet"
-      ></link>
-    </Head>
-
-    <div>
-      <h1>Dio lurido</h1>
-      <style jsx>
-        {`
-          div {
-            background: #333;
-          }
-
-          h1 {
-            background: #222;
-          }
-        `}
-      </style>
-    </div>
-  </>
+  <div>
+    <h1>Dio lurido</h1>
+  </div>
 );
 
 export default Index;
