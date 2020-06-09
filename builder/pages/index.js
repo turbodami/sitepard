e5e8201865660472162802fc1b4eabca5c0fc22c
turbodami <presadamiano@gmail.com>
2020-06-09 19:33:47 +0200

    lurido dio

---
+++ builder/pages/index.js
@@ -0,0 +1,18 @@
+const Index = () => (
+  <div>
+    <h1>Dio lurido</h1>
+    <style jsx>
+      {`
+        div {
+          background: #333;
+        }
+
+        h1 {
+          background: #222;
+        }
+      `}
+    </style>
+  </div>
+);
+
+export default Index;
