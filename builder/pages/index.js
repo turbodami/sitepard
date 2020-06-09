13b9f31ac0c82083a2c9a5d5380c69c935c1ed79
turbodami <presadamiano@gmail.com>
2020-06-09 20:10:17 +0200

    dio losco

--- builder/pages/index.js
+++ builder/pages/index.js
@@ -1,4 +1,11 @@
+import Head from 'next/head';
+
 const Index = () => (
+    <Head>
+         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/normalize/7.0.0/normalize.css" />
+      <link href="https://fonts.googleapis.com/css?family=Raleway" rel="stylesheet"></link>
+
+    </Head>
   <div>
     <h1>Dio lurido</h1>
     <style jsx>
