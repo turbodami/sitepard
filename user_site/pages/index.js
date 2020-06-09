8be926330495326657a8252b2009263c9b6a5268
turbodami <presadamiano@gmail.com>
2020-06-09 18:36:00 +0200

    dio lazzaro

--- user_site/pages/index.js
+++ user_site/pages/index.js
@@ -1,8 +1,24 @@
-import Link from "next/link";
+// pages/index.js
 
-export default function Index() {
-  return <h1>Porco dio</h1>;
+function Home() {
+  return (
+    <div className="container">
+      <h1>Hello Next.js</h1>
+      <p>Let's explore different ways to style Next.js apps</p>
+      <style jsx>{`
+        .container {
+          margin: 50px;
+        }
+        p {
+          color: blue;
+        }
+      `}</style>
+    </div>
+  );
 }
+
+export default Home;
+
 /* Index.getInitialProps = async ({ query: { diocan } }) => {
   //console.log(diocan);
   return {
