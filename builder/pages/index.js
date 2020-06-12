130308d3df2640f6d3f7c02f304fa7a13d4375f3
turbodami <presadamiano@gmail.com>
2020-06-12 11:22:14 +0200

    props passed

--- builder/pages/index.js
+++ builder/pages/index.js
@@ -4,6 +4,16 @@ const Title = styled.h1`
   font-size: 50px;
 `;
 
-export default function Home() {
-  return <Title>My page</Title>;
+export default function Home(props) {
+  return (
+    <p>{props.diocan.category}</p>
+    <Title>My page</Title>;
+
+  )
+}
+
+Home.getInitialProps({query: {diocan}}){
+  return {
+    diocan: diocan
+  }
 }
