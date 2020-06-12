83ce04f0f3c6f0946e98ff60a3712a639b13740b
turbodami <presadamiano@gmail.com>
2020-06-12 11:30:07 +0200

    dio lemure

--- builder/pages/index.js
+++ builder/pages/index.js
@@ -1,21 +1,23 @@
 import styled from "styled-components";
-import React, {Fragment} from "react";
+import React, { Fragment } from "react";
 
 const Title = styled.h1`
   font-size: 50px;
 `;
 
-export default function Home(props) {
+const Home = (props) => {
   return (
     <Fragment>
       <p>{props.diocan.category}</p>
       <Title>My page</Title>;
     </Fragment>
-  )
-}
+  );
+};
 
-Home.getInitialProps({query: {diocan}}) => {
+Home.getInitialProps = ({ query: { diocan } }) => {
   return {
-    diocan: diocan
-  }
-}
+    diocan: diocan,
+  };
+};
+
+export default Home;
