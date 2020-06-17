800a954a18302ff0dc935d0dfe775cbe068d4c72
nicolaramoso@gmail.com <nicolaramoso@gmail.com>
2020-06-17 22:04:18 +0200

    Piccole correzioni

--- routes/api/mapsRoute.js
+++ routes/api/mapsRoute.js
@@ -21,14 +21,13 @@ router.get('/get-coordinates/:address', async (req ,res)=>
                     const parsed = new GeoCodeParser(JSON.parse(body));
                     res.status(200).json({lat : parsed.getLat(), lng: parsed.getLng()});
                 }
-            }) 
+            }); 
         }
          catch (error) 
         {
             console.log(error);
             res.status(500).json({message: "Errore nella richiesta"});
-        }
-        
+        }  
     }
 );
 
