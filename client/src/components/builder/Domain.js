96698d3b99abfa9ee4d3715259d9176a5cf267e9
turbodami <presadamiano@gmail.com>
2020-07-08 14:59:37 +0200

    domain

--- client/src/components/builder/Domain.js
+++ client/src/components/builder/Domain.js
@@ -1,64 +1,48 @@
 import React, { Fragment } from "react";
+import { useSpring, animated } from "react-spring";
+import Nav from "./Nav";
 
-const Domain = ({ handleSelection }) => {
+const Domain = ({ handleSelection, prevStep }) => {
+  const props = useSpring({
+    opacity: 1,
+    from: { opacity: 0 },
+    config: { duration: 500 },
+  });
+  
   return (
-    <Fragment>
-      <h1 className="large text-primary">
-        Che tipo di dominio vuoi utilizzare?
-      </h1>
-      <p className="lead">
-        <i className="fas fa-user" /> Puoi scegliere tra sottodominio e dominio
-      </p>
-
-      <div className="profile-grid my-1">
-        <div className="profile-exp bg-white p-2">
-          <h2 className="text-primary">Sottodominio</h2>
-          <div>
-            <p className="text-dark">
-              Il tuo sito verrà pubblicato come un sottodominio della nostra
-              piattaforma. (Scelta consigliata)
-              <br />
-              Ad esempio:
-              <br />
-              <strong>pizzeriabellanapoli.potoroo.it</strong>
-            </p>
+    <animated.div style={props}>
+      <Fragment>
+        <section className="section">
+          <div className="container">
+            <Nav prevStep={prevStep} />
+            <div className="columns is-centered">
+              <div className="column is-3"></div>
+              <div className="column is-6 has-text-centered">
+                <p className="title is-1">Che tipo di dominio vuoi utilizzare?</p>
+                <p className="subtitle is-3">
+                  Puoi scegliere tra sottodominio e dominio
+                </p>
+                <input
+                  type="button"
+                  onClick={(e) => handleSelection(e)}
+                  className="btn btn-primary"
+                  name="type"
+                  value="subdomain"
+                />
+                <input
+                  type="button"
+                  onClick={(e) => handleSelection(e)}
+                  className="btn btn-primary"
+                  name="type"
+                  value="domain"
+                />
+              </div>
+              <div className="column is-3"></div>
+            </div>
           </div>
-          <input
-            type="button"
-            onClick={(e) => handleSelection(e)}
-            className="btn btn-primary"
-            name="type"
-            value="subdomain"
-          />
-        </div>
-
-        <div className="profile-edu bg-white p-2">
-          <h2 className="text-primary">Dominio</h2>
-          <div>
-            <p className="text-dark">
-              Il tuo sito verrà pubblicato sul tuo dominio personale. (Scegli
-              questa opzione se già possiedi un dominio)
-              <br />
-              Ad esempio:
-              <br />
-              <strong>pizzeriabellanapoli.it</strong>
-            </p>
-          </div>
-          <input
-            type="button"
-            onClick={(e) => handleSelection(e)}
-            className="btn btn-primary"
-            name="type"
-            value="domain"
-          />
-        </div>
-      </div>
-      <br />
-      <p className="lead">
-        <i className="fas fa-user" /> Verrai contattato entro 1 ora per definire
-        tutti i dettagli e pubblicare il sito.
-      </p>
-    </Fragment>
+        </section>
+      </Fragment>
+    </animated.div>
   );
 };
 
