a1412b4abf4576a43eb2e6307bb86a51be2a9620
turbodami <presadamiano@gmail.com>
2020-07-10 23:37:22 +0200

    nicola bellissimo uomo

---
+++ .VSCodeCounter/results.md
@@ -0,0 +1,48 @@
+# Summary
+
+Date : 2020-07-09 19:37:22
+
+Directory /home/turbodami/Code/deploi
+
+Total : 86 files,  26657 codes, 159 comments, 748 blanks, all 27564 lines
+
+[details](details.md)
+
+## Languages
+| language | files | code | comment | blank | total |
+| :--- | ---: | ---: | ---: | ---: | ---: |
+| JSON | 8 | 19,047 | 0 | 8 | 19,055 |
+| JavaScript | 72 | 5,012 | 159 | 732 | 5,903 |
+| CSS | 2 | 2,509 | 0 | 2 | 2,511 |
+| Pug | 3 | 89 | 0 | 5 | 94 |
+| Markdown | 1 | 0 | 0 | 1 | 1 |
+
+## Directories
+| path | files | code | comment | blank | total |
+| :--- | ---: | ---: | ---: | ---: | ---: |
+| . | 86 | 26,657 | 159 | 748 | 27,564 |
+| client | 58 | 20,364 | 38 | 488 | 20,890 |
+| client/src | 56 | 5,945 | 38 | 486 | 6,469 |
+| client/src/actions | 4 | 388 | 25 | 95 | 508 |
+| client/src/components | 42 | 5,369 | 8 | 355 | 5,732 |
+| client/src/components/builder | 17 | 1,204 | 1 | 133 | 1,338 |
+| client/src/components/dashboard | 7 | 728 | 0 | 87 | 815 |
+| client/src/components/layout | 4 | 209 | 6 | 28 | 243 |
+| client/src/components/pages | 6 | 390 | 1 | 57 | 448 |
+| client/src/components/routing | 2 | 48 | 0 | 11 | 59 |
+| client/src/components/show | 3 | 136 | 0 | 19 | 155 |
+| client/src/components/site-forms | 2 | 145 | 0 | 19 | 164 |
+| client/src/reducers | 4 | 111 | 0 | 16 | 127 |
+| client/src/utils | 1 | 9 | 0 | 3 | 12 |
+| config | 2 | 22 | 1 | 6 | 29 |
+| controllers | 2 | 143 | 16 | 33 | 192 |
+| middleware | 1 | 15 | 3 | 5 | 23 |
+| models | 2 | 150 | 0 | 8 | 158 |
+| renderer | 10 | 1,290 | 54 | 31 | 1,375 |
+| renderer/config | 2 | 22 | 1 | 6 | 29 |
+| renderer/models | 2 | 137 | 4 | 8 | 149 |
+| renderer/views | 3 | 89 | 0 | 5 | 94 |
+| routes | 7 | 1,054 | 44 | 167 | 1,265 |
+| routes/api | 7 | 1,054 | 44 | 167 | 1,265 |
+
+[details](details.md)
---
+++ client/src/components/show/ProductsList.js
@@ -0,0 +1,73 @@
+import React, { Fragment, useState } from "react";
+import PropTypes from "prop-types";
+import { connect } from "react-redux";
+import { deleteProduct, deleteCategory } from "../../actions/site";
+import AddProduct from "../site-forms/AddProduct";
+
+const ProductsList = ({ categories, deleteCategory, products, deleteProduct }) => {
+  const [modProdIsActive, toggleModProd] = useState(false);
+  
+  const list = categories.map((cat) => (
+    <Fragment>
+      <div className={ modProdIsActive? `modal is-active` : `modal`}>
+            <div className="modal-background" onClick={() => toggleModProd(!modProdIsActive)}></div>
+            <div className="modal-content">
+              <div className="box">
+                <AddProduct cat={cat}/>
+              </div>
+            </div>
+            <button className="modal-close is-large" aria-label="close" onClick={() => toggleModProd(!modProdIsActive)}></button>
+      </div>
+      <div className="container">
+        <table className="table">
+          <thead>
+            <tr key={cat._id}>
+              <th>{cat.name}</th>
+              <th>
+                <div className="buttons">
+                  <button className="button is-danger"
+                    onClick={() => deleteCategory(cat._id)}
+                  >
+                    Elimina
+                  </button>
+                
+                  <button className="button is-primary" onClick={() => toggleModProd(!modProdIsActive)}>Aggiungi pizza</button>
+                </div>
+              </th>
+              
+            </tr>
+          </thead>
+          <tbody>
+            {products.map(function(prod) {
+
+              if (prod.category === cat.name) {
+                return (
+                  <tr key={prod._id}>
+                    <td>
+                      <button className="delete is-small" aria-label="close" onClick={() => deleteProduct(prod._id)}></button>
+                    </td>
+                    <td>{prod.name}</td>
+                    <td>{prod.description}</td>
+                    
+                    <td>{prod.price}</td>
+                    
+                    
+                  </tr>
+                );
+              }
+            })}
+          </tbody>
+        </table>
+      </div>
+    </Fragment>
+  ));
+
+  return <Fragment>{list}</Fragment>;
+};
+
+ProductsList.propTypes = {
+  deleteCategory: PropTypes.func.isRequired,
+  deleteProduct: PropTypes.func.isRequired,
+};
+
+export default connect(null, { deleteCategory, deleteProduct })(ProductsList);
