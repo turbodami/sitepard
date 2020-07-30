c4fdf6be26d68e90c0e79619c727bc4cc06dfef8
turbodami <presadamiano@gmail.com>
2020-07-30 11:12:35 +0200

    casin

--- client/src/reducers/site.js
+++ client/src/reducers/site.js
@@ -32,7 +32,7 @@ export default function (state = initialState, action) {
       return {
         ...state,
         site: null,
-        loading: false,
+        loading: true,
       };
     default:
       return state;
