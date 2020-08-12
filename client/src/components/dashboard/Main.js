9b89d6e169ecde19532bd06dd048c6edc08ab01f
turbodami <presadamiano@gmail.com>
2020-08-12 22:35:08 +0200

    daisnd

--- client/src/components/dashboard/Main.js
+++ client/src/components/dashboard/Main.js
@@ -40,9 +40,9 @@ const Main = ({ getCurrentSite, auth: { user }, site: { site, loading } }) => {
           </p>
 
           <p>Il link del tuo sito è: </p>
-          <Link to={`http://${site.domain}.sitepard.com`}> 
+          <a href={`http://${site.domain}.sitepard.com`}> 
             <h2 className="subtitle is-4">http://{site.domain}.sitepard.com</h2>
-          </Link>
+          </a>
           <br />
           <br />
           <br />
