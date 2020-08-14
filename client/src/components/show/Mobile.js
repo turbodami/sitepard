b397fb4cf980e95d0bc95ddd4e9d635bea139b70
turbodami <presadamiano@gmail.com>
2020-08-14 19:18:41 +0200

    ok forse va

--- client/src/components/show/Mobile.js
+++ client/src/components/show/Mobile.js
@@ -40,7 +40,7 @@ const Mobile = ({getCurrentSite, site: { site },}) => {
         <div className="screen">
           <iframe
             id="preview"
-            src={"http://" + site.subdomain + ".cactusdomaindev.xyz"}
+            src={"http://" + site.subdomain + ".sitepard.xyz"}
             title="your website"
             height="100%"
             width="100%"
