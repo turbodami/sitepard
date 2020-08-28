1ccd29b482c86b121d1829e0d7f66a30721dc9a0
turbodami <presadamiano@gmail.com>
2020-08-28 10:03:32 +0200

    primo tentativo bislacco

--- client/src/actions/site.js
+++ client/src/actions/site.js
@@ -50,8 +50,11 @@ export const uploadLogo = (formData, logo) => async (
     
     const extension = uploadedName.split('.').pop();
 
-    const res = await axios.post(`/api/siteSpaces/image/${subdomain}${imageName}logo.${extension}`, data, config);
-
+    const res = await axios.post(`/api/siteSpaces/image/${subdomain}${imageName}logo.${extension}`, data, config)
+      .then((response) => {
+        formData.logo = response.data.link;
+      });
+    
     dispatch({
       type: IMAGE_UPLOADED,
     });
