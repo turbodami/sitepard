84838695cafe99178689cd97d25524d4dc6312fb
turbodami <presadamiano@gmail.com>
2020-09-17 16:32:56 +0200

    jsjdfj

--- routes/api/pay.js
+++ routes/api/pay.js
@@ -1,15 +1,14 @@
-const cors = require("cors");
 const express = require('express');
 const app = express.Router();
+const cors = require("cors");
 
 //const stripe= require("stripe")("");
 const uuid = require("uuid/v4");
 
 //middleware
-app.use(express.json());
 app.use(cors());
 
 //routes
-app.get("/", (req, res) => {
+app.get('/', (req, res) => {
     res.send("It works");
 })
