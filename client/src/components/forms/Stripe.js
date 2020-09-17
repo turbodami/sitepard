d5385c140e1b83f69e864bd589bcc35b514ea4c0
turbodami <presadamiano@gmail.com>
2020-09-17 22:45:39 +0200

    jsdfj

--- client/src/components/forms/Stripe.js
+++ client/src/components/forms/Stripe.js
@@ -1,5 +1,6 @@
 import React, { Fragment, useState } from "react";
 import StripeCheckout from "react-stripe-checkout";
+import Axios from "axios";
 
 const Stripe = () => {
     const [product, setProduct] = useState({
@@ -8,12 +9,33 @@ const Stripe = () => {
         productBy: "sitepard"
     });
 
+    const makePayment = token => {
+        const body = {
+            token,
+            product
+        };
+        const config = {
+            headers: {
+              "Content-Type": "application/json",
+            },
+          };
+
+        return axios.post('/api/pay/payment', body, config)
+            .then(response => {
+                console.log("RESPONSE ", response);
+                const { status } = response;
+                console.log("STATUS ", status);
+            })
+            .catch(error => console.log(error));
+    }
+
     return (
         <Fragment>
             <StripeCheckout 
-                stripeKey=""
-                token=""
+                stripeKey={process.env.REACT_APP_KEY}
+                token={makePayment}
                 name="Paga ora"
+                amount={product.price * 100}
             />
         </Fragment>
     );
