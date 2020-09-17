4ebf8e20edaaac15f08ec4fbec0d9287317bd7ae
turbodami <presadamiano@gmail.com>
2020-09-17 22:16:04 +0200

    nsdnf

--- routes/api/pay.js
+++ routes/api/pay.js
@@ -3,8 +3,8 @@ const router = express.Router();
 require('dotenv').config();
 const cors = require("cors");
 
-//const stripe= require("stripe")("");
-//const uuid = require("uuid/v4");
+const stripe= require("stripe")("pk_test_5kEss9zsrNnzTEmgT1DMA0Mx00StBPnE3c");
+const uuid = require("uuid/v4");
 
 //middleware
 router.use(express.json());
@@ -13,6 +13,29 @@ router.use(cors());
 //routes
 router.get('/', (req, res) => {
     res.send("It works");
+});
+
+router.post('/payment', (req, res) => {
+    const { product, token } = req.body;
+    console.log("PRODUCT ", product);
+    console.log("PRICE ", product.price);
+    const idempontencyKey = uuid();
+
+    return stripe.customers.create({
+        email: token.email,
+        source: token.id
+    })
+    .then(customer => {
+        stripe.charges.create({
+            amount: product.price * 100,
+            currency: 'eur',
+            customer: customer.id,
+            receipt_email: token.email,
+            description: `purchase of ${product.name}`
+        }, {idempontencyKey})
+    })
+    .then(result => res.status(200).json(result))
+    .catch(err => console.log(err));
 })
 
 module.exports = router;
