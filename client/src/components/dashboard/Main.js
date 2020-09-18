d8996a87eb3dccf92cd6592b169fcd20149fcc0d
turbodami <presadamiano@gmail.com>
2020-09-18 12:09:00 +0200

    jsdfj

--- client/src/components/dashboard/Main.js
+++ client/src/components/dashboard/Main.js
@@ -46,7 +46,8 @@ const Main = ({ getCurrentSite, auth: { user }, site: { site, loading } }) => {
           </a>
           <br />
           <br />
-          <p className="subtitle is-4">Paga entro 48 ore per attivare il servizio. Tranquillo! Non ci sarà alcun rinnovo automatico, ti contatteremo personalmente tra 30 giorni per capire insieme se il servizio è stato di tuo gradimento.</p>
+          <p className="subtitle is-4">Paga entro 48 ore per attivare il servizio. Sono 20,00€ per 30 giorni.</p>
+          <p className="subtitle is-5">Tranquillo! Non ci sarà alcun rinnovo automatico, ti contatteremo personalmente tra 30 giorni per capire insieme se il servizio è stato di tuo gradimento.</p>
           <Stripe />
           <br />
           <br />
