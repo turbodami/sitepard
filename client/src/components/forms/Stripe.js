794a83b6bfe744ec2007cc83973cf37e2de2dd0f
turbodami <presadamiano@gmail.com>
2020-09-18 17:09:53 +0200

    sjdfj

--- client/src/components/forms/Stripe.js
+++ client/src/components/forms/Stripe.js
@@ -33,20 +33,21 @@ const Stripe = () => {
     return (
         <Fragment>
             <div className="box has-background-primary">
-            <p className="subtitle is-4">Paga entro 48 ore per attivare il servizio. Sono 20,00€ per 30 giorni.</p>
-            <p className="subtitle is-5">Tranquillo! Non ci sarà alcun rinnovo automatico, ti contatteremo personalmente tra 30 giorni per capire insieme se il servizio è stato di tuo gradimento.</p>
-            <StripeCheckout 
-                stripeKey="pk_test_5kEss9zsrNnzTEmgT1DMA0Mx00StBPnE3c"
-                token={makePayment}
-                name="Paga ora"
-                amount={product.price * 100}
-                currency="EUR"
-                panelLabel="Paga "
-                locale="it"
-                allowRememberMe= {false}
-            >
-                <button className="button is-info">Paga ora</button>
-            </StripeCheckout>
+                <p className="subtitle is-4">Paga entro 48 ore per attivare il servizio. Sono 20,00€ per 30 giorni.</p>
+                <p className="subtitle is-5">Tranquillo! Non ci sarà alcun rinnovo automatico, ti contatteremo personalmente tra 30 giorni per capire insieme se il servizio è stato di tuo gradimento.</p>
+                <StripeCheckout 
+                    stripeKey="pk_test_5kEss9zsrNnzTEmgT1DMA0Mx00StBPnE3c"
+                    token={makePayment}
+                    name="Paga ora"
+                    amount={product.price * 100}
+                    currency="EUR"
+                    panelLabel="Paga "
+                    locale="it"
+                    allowRememberMe= {false}
+                >
+                    <button className="button is-info">Paga ora</button>
+                </StripeCheckout>
+            </div>
         </Fragment>
     );
 }
